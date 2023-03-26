df3dfc7c24103fad6fb61b4886b88518b2a12db8
cheraph#6290 <125369139+cheraphdev@users.noreply.github.com>
2023-03-26 13:57:30 +0200

    Add files via upload

---
+++ config/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    bot: require("./bot.json"),
+    app: require("./app.json"),
+    db: require("./db.json"),
+    channels: require("./channels.json"),
+    colors: require("./colors.json"),
+    emojis: require("./emojis.json"),
+    roles: require("./roles.json"),
+    webhooks: require("./webhooks.json"),
+    support: "https://discord.gg/fPbVS3GNBp",
+    topgg: "https://google.com",
+    guildid: "",
+    ownersId: [
+        "1056780751133224990" // cheraph#6290
+    ]
+};
