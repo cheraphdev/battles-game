8e65a3997bc5c61527b69c82b9382e7f4a93924a
cheraph#6290 <125369139+cheraphdev@users.noreply.github.com>
2023-03-28 23:32:27 +0200

    Add files via upload

--- event/messageCreate.js
+++ event/messageCreate.js
@@ -32,7 +32,7 @@ module.exports = {
             data.save();
         }
 
-        const randomNumber = Math.floor(Math.random() * 30);
+        const randomNumber = Math.floor(Math.random() * 10);
         if (randomNumber === 0) {
 
             let data = new messagesSchema({
